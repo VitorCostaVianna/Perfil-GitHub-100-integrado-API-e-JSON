a1bd63be23937da9598d27ffd48bb12eb8842c87
Vitor Costa Vianna <vcvianna@sga.pucminas.br>
2024-06-16 10:34:57 -0300

    REPOSITORIO PAGE

---
+++ public/assents/js/repo.js
@@ -0,0 +1,36 @@
+const repositories = document.querySelector('body');
+
+function getApiGitHub() {
+    fetch('https://api.github.com/users/VitorCostaVianna/repos')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+            data.map(item => {
+                let project = document.createElement('div');
+
+                project.innerHTML = `
+                <div class="project">
+                    <div>
+                        <h4 class="title">${item.name}</h4>
+                        <span class="data-create">${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+                    </div>
+                    <div>
+                        <a href="${item.html_url}" target="_blank">${item.html_url}</a>
+                        <spam class="language"><span class="circle"></span>${item.language}</spam>
+                    </div>
+                </div>
+                `
+
+                repositories.appendChild(project);
+            })
+        })
+}
+
+
+
+window.onload = () => {
+    getApiGitHub();
+};
